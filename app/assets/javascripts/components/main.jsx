771ef0581b478b7c88bf820018d912b847fd0bab
aydar0894 <aydar0894@mail.ru>
2017-03-25 02:07:27 +0300

    land

---
+++ app/assets/javascripts/components/main.jsx
@@ -0,0 +1,24 @@
+class Main extends React.Component{
+
+  render(){
+    return (
+    	<div>
+    	<form>
+
+    	<div class="uk-margin">
+        <input class="uk-input uk-form-width-medium uk-form-large" type="text" placeholder="Large"/>
+   		</div>
+
+    	<div class="uk-margin">
+        <input class="uk-input uk-form-width-medium" type="text" placeholder="Default"/>
+    	</div>
+
+   		 <div class="uk-margin">
+        <input class="uk-input uk-form-width-medium uk-form-small" type="text" placeholder="Small"/>
+    	</div>
+
+		</form>
+    	</div>
+    	)
+  }
+}
